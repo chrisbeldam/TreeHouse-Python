1cf5730f74a6b945d0944436e3e3c78561ba4357
Chris Beldam <chrisgbeldam@gmail.com>
2017-05-12 10:04:13 +0100

    Python for beginners

---
+++ Python Basics/pythonforbeginners.py
@@ -0,0 +1,28 @@
+
+# Variables
+
+lunch = "turkey sandwich" # Stores this data in the variable
+a = 10 # Int
+b = 2.5 # Float
+
+name = "Chris Beldam"
+
+favourite_fruit = "apple"
+
+
+# Functions & Objects
+
+def answer_the_phone():
+    print("Hello?")
+
+answer_the_phone()
+
+def say_hello():
+    print("Hello!")
+
+say_hello()
+
+def name():
+    print("my name is Chris!")
+
+name():
