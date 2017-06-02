90c03a2df853206d37ce826279127f40002188d3
Chris Beldam <chrisgbeldam@gmail.com>
2017-06-02 09:55:55 +0100

    Some Test Work

---
+++ Python Testing/testing.py
@@ -0,0 +1,26 @@
+# Quantative Assertions
+
+import unittest
+
+import moves
+
+class MoveTests(unittest.TestCase):
+    def setUp(self): # Allows us to write less code
+        self.rock = moves.Rock()
+        self.paper = moves.Paper()
+        self.scissors = moves.Scissors()
+
+    def test_five_plus_five(self):
+        assert 5+ 5 == 10
+
+    def test_one_plus_one(self):
+        assert not 1+1 == 3
+
+    def test_equal(self):
+        self.assertEqual(self.rock, moves.Rock())
+
+    def test_not_equal(self):
+        self.assertNotEqual(self.rock,self.paper)
+
+if __name__ = '__main__':
+    unittest.main()
